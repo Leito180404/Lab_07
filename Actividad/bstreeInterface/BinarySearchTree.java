97fcf6a83354b52cdf4a9b3a8a66426fc7e1451e
Leito180404 <leonardo.lonasco@estudiante.ucsm.edu.pe>
2025-05-12 08:33:24 -0500

    Update BinarySearchTree.java

--- Actividad/bstreeInterface/BinarySearchTree.java
+++ Actividad/bstreeInterface/BinarySearchTree.java
@@ -1,5 +1,12 @@
 package Actividad.bstreeInterface;
 
+import Actividad.Exceptions.ExceptionEmpty;
+import Actividad.Exceptions.ItemDuplicated;
+import Actividad.Exceptions.ItemNoFound;
+
 public interface BinarySearchTree<E> {   
-    
+    void insert(E data) throws ItemDuplicated;
+    E search(E data) throws ItemNoFound;
+    void delete(E data) throws ExceptionEmpty;
+    boolean isEmpty();
 }
